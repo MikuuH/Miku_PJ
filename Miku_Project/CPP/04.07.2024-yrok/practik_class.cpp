26b76972db35e5d42e6ba3b3363e39f9d7e9959c
Miku <MikuMiku@mail.com>
2024-04-07 17:22:19 +0300

    modified:   Miku_Project/CPP/04.07.2024-yrok/practik_class.cpp modified:   Miku_Project/CPP/04.07.2024-yrok/practik_class.exe

--- Miku_Project/CPP/04.07.2024-yrok/practik_class.cpp
+++ Miku_Project/CPP/04.07.2024-yrok/practik_class.cpp
@@ -1,38 +1,33 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 class account {
-    private:
-        int dateOfBirth;
-        int inn;
-        int cvv;
-        int term;
-        string isLoan = "no";
-        string IsBlocked = "no";
+private:
+    int dateOfBirth;
+    int inn;
+    int cvv;
+    int term;
+    string isLoan = "no";
+    string IsBlocked = "no";
 
-    public:
-        string name;
-        string surname;
-        int balance = 0;
-        int cardNumber;
+public:
+    string name;
+    string surname;
+    int balance = 0;
+    int cardNumber;
 
-        account(
-        string n, string s, int b, int c) {
-            name = n;
-            surname = s;
-            balance = b;
-            cardNumber = c;
-    };
+    account(string n, string s, int b, int c) : name(n), surname(s), balance(b), cardNumber(c) {}
 
-    void infoAccountPublic() const {
-        cout << "Name:" << name << endl;
+    void infoAccountPublic() {
+        cout << "Name: " << name << endl;
         cout << "Surname: " << surname << endl;
         cout << "Balance: " << balance << endl;
         cout << "CardNumber: " << cardNumber << endl;
     }
 
-    void infoAccountPrivate() const {
+    void infoAccountPrivate() {
         cout << "dateOfBirth: " << dateOfBirth << endl;
         cout << "inn: " << inn << endl;
         cout << "cvv: " << cvv << endl;
@@ -50,19 +45,18 @@ class account {
     }
 
     bool moneyTransfer(double b) {
-        if (b >= balance) {
+        if (b <= balance) { // Исправлено условие
             balance -= b;
             return true;
         } else {
-            cout << "Don't you have anything to do? You don't have any money. Sit here and eat your lollipop!!!" << endl;
+            cout << "Insufficient funds" << endl;
             return false;
         }
     }
 
     int editCardNumber(int newcard) {
         cardNumber = newcard;
-
-
+        return cardNumber; // Добавлено возвращаемое значение
     }
 };
 
@@ -75,14 +69,14 @@ int main() {
     int cardNumber = 55555;
 
     int choice;
-    while (true)
-    {
+    while (true) {
         cout << "please enter choice: " << endl;
         cin >> choice;
         if (choice == 0) {
             if (myAccount != nullptr) {
                 int coutAccount;
                 cout << "You already have an account. Are you sure you want to create a new one? 1 | 0" << endl;
+                cin >> coutAccount; // Исправлено
                 if (coutAccount == 1) {
                     myAccount = new account(name, surname, balanceCard, cardNumber);
                 } else {
@@ -90,50 +84,58 @@ int main() {
                 }
             }
         }
-        else if (choice == 1)
-        {
-            myAccount->infoAccountPublic();
+        else if (choice == 1) {
+            if (myAccount != nullptr) {
+                myAccount->infoAccountPublic();
+            } else {
+                cout << "You don't have an account." << endl;
+            }
         }
         else if (choice == 2) {
-            cout << "Enter amount: ";
-            cin >> balanceCard;
-            myAccount->AddBalance(balanceCard);
-        }
-        else if (choice == 3)
-        {
-            cout << "enter how much money you want to transfer: " << endl;
-            cout << "number of the card you want to transfer to: " << endl;
-            cin >> balanceCard;
-            int transfer = myAccount->moneyTransfer(balanceCard);
-
-            if (transfer) {
-                cout << "the money was successfully transferred" << endl;
-            }
-        }
-        else if (choice == 4)
-        {
-            string newcard;
-            cout << "enter new number curd. Enter 5 numbers" << endl;
-            if (newcard.length() <= 5) {
-                cout << "NONONONOONONNO IS NOT 5 LENGHT BRAT" << endl;
+            if (myAccount != nullptr) {
+                cout << "Enter amount: ";
+                cin >> balanceCard;
+                myAccount->AddBalance(balanceCard);
             } else {
-                cin >> newcard;
-                int newCardInt = stoi(newcard);
-                myAccount->editCardNumber(newCardInt);
+                cout << "You don't have an account." << endl;
             }
         }
-        else if (choice == 10)
-        {
+        else if (choice == 3) {
+            if (myAccount != nullptr) {
+                cout << "Enter amount to transfer: ";
+                cin >> balanceCard;
+                bool transfer = myAccount->moneyTransfer(balanceCard);
+                if (transfer) {
+                    cout << "The money was successfully transferred." << endl;
+                }
+            } else {
+                cout << "You don't have an account." << endl;
+            }
+        }
+        else if (choice == 4) {
+            if (myAccount != nullptr) {
+                string newcard;
+                cout << "Enter new card number (5 digits): ";
+                cin >> newcard;
+                if (newcard.length() != 5) {
+                    cout << "Invalid card number length." << endl;
+                } else {
+                    int newCardInt = stoi(newcard);
+                    myAccount->editCardNumber(newCardInt);
+                }
+            } else {
+                cout << "You don't have an account." << endl;
+            }
+        }
+        else if (choice == 10) {
             cout << "GoodBye!" << endl;
             break;
         }
     }
 
-
-
-
     if (myAccount != nullptr) {
         myAccount->infoAccountPublic();
+        delete myAccount; 
     }
 
     return 0;
