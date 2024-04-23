6fa0e657a7bdebf4076ce7c6bb466ce1522c5623
Miku <39mkui@gmail.com>
2024-04-23 14:54:15 +0300

    deleted:    .vscode/c_cpp_properties.json deleted:    .vscode/compile_commands.json deleted:    .vscode/launch.json deleted:    .vscode/settings.json deleted:    .vscode/tasks.json modified:   Miku_Project/JavaScript/main.js deleted:    Miku_Project/JavaScript/main.js

--- Miku_Project/JavaScript/main.js
+++ Miku_Project/JavaScript/main.js
@@ -1,1 +1,30 @@
-console.log(123);
+/**
+ * Перепишите следующий код через switch-case:
+ * let lang = 'ru';
+
+if (lang == 'ru') {
+	console.log('рус');
+} else if (lang == 'en') {
+	console.log('анг');
+} else if (lang == 'de') {
+	console.log('нем');
+} else {
+	console.log('язык не поддерживается');
+}
+ */
+
+let lang = 'ru';
+
+switch (lang) {
+    case 'ru':
+        console.log('russian');
+    break;
+    case 'en':
+        console.log(eng);
+    break;
+    case 'de':
+        console.log('nem');
+    break;
+    default:
+        console.log("Incorrect lang");
+}
