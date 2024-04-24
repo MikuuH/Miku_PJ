70100a1aa53497773f5c9aa4a7ced231e53d5e54
Miku <39mkui@gmail.com>
2024-04-24 12:45:52 +0300

    modified:   "Miku_Project/JavaScript/\320\277\321\200\320\260\320\272\321\202\320\270\320\272\320\260/23.04.2024.js" new file:   "Miku_Project/JavaScript/\321\202\320\265\320\276\321\200\320\270\321\217/23.04.2024.js"

--- Miku_Project/JavaScript/практика/23.04.2024.js
+++ Miku_Project/JavaScript/практика/23.04.2024.js
@@ -15,18 +15,47 @@ if (lang == 'ru') {
 
 
 // ПРАВИЛЬНОЕ РЕШЕНИЕ //
-let lang = 'ru';
+//let lang = 'ru';
 
-switch (lang) {
-    case 'ru':
-        console.log('russian');
-    break;
-    case 'en':
-        console.log(eng);
-    break;
-    case 'de':
-        console.log('nem');
-    break;
-    default:
-        console.log("Incorrect lang");
+// switch (lang) {
+//     case 'ru':
+//         console.log('russian');
+//     break;
+//     case 'en':
+//         console.log(eng);
+//     break;
+//     case 'de':
+//         console.log('nem');
+//     break;
+//     default:
+//         console.log("Incorrect lang");
+// }
+
+
+// если число положительное - делать отрицательным. 
+// function makeNegative(num) {
+//     return num > 0 ? -num : num;
+//    }
+
+//  console.log(makeNegative(-10));
+
+
+/**
+ * Суммируйте все числа данного массива ( cq. list ), кроме самого высокого и самого низкого элемента (по значению, а не по индексу! ).
+
+Наибольший или наименьший элемент соответственно - это по одному элементу на каждом ребре, даже если существует более одного элемента с одинаковым значением.
+ */
+function sumArray(array) {
+    if (!array || array.length <= 0) {
+        return 0;
+    }
+
+    var min = Math.min.apply(null, array),
+        max = Math.max.apply(null, array);
+
+    return  array.filter(item <= item !== min && item !== max)
+           .array.reduce((total, current) => total + current, 0);
 }
+
+let arr = [1, 2, 3];
+console.log(sumArray(array=arr));
