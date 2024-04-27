b9668c1303132c12351f2a8a6d034c19192744db
Miku <39mkui@gmail.com>
2024-04-27 16:47:48 +0300

    new file:   Miku_Project/CPP/31.03.2024/DZ/DZ2/MaxValue.cpp

---
+++ Miku_Project/CPP/31.03.2024/DZ/DZ2/MaxValue.cpp
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Возвращает наибольшее число
+template <typename T>
+T maxValue(T one, T two) {
+    return (one > two) ? one : two;
+}
+
+// показывает все эелементы массива
+template <typename T>
+void printArray(T arr, T size_arr) {
+    for (int i = 0; i < size_arr; i++) {
+        cout << arr[i] << "";
+
+    }
+    return 0;
+}
+
+int main() {
+    // // maxValue
+    // cout << "maxValue: INT: " << maxValue<int>(1, 10) << endl;
+    // cout << "maxValue: FLOAT: " << maxValue<float>(7.9, 1.9) << endl;
+    // cout << "maxValue: CHAR: " << maxValue<string>("люблю", "МИКУ");
+
+    const int size_arr = 5;
+    int arr[size_arr] = {1, 2, 3, 4, 5};
+    cout << "printArray" << printArray(arr, size_arr) << endl;
+
+
+    return 0;
+}
