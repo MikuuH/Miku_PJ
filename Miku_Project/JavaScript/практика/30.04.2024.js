87379353ad8460b19f2456a9b6d32198142815cb
Miku <39mkui@gmail.com>
2024-04-30 13:48:02 +0300

    создан новый файл для практики за 30.04.2024

---
+++ Miku_Project/JavaScript/практика/30.04.2024.js
@@ -0,0 +1,97 @@
+// циклы
+
+// 1
+// function printAllElementMassiv(list) {
+//     for (let elem of list) {
+//         console.log(elem);
+//     }
+// }
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// printAllElementMassiv(list=arr)
+
+// 2
+// function printElementObject(obj) {
+
+//     for (let key in obj) {
+//         console.log(obj[key]); // выведет элемент ключа
+//         console.log(key);  // выведет ключ
+//     }
+//     return 0;
+// }
+
+// let obj = {x: 1, y: 2, z: 3};
+// printElementObject(obj=obj)
+
+// 3
+// генерация от 1 до 100
+// let i = 1
+// while (i <= 100) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// 4
+// от 11 до 33
+// let i = 11
+// while (i <= 33) {
+//     console.log(i);
+//     i++;
+// }
+
+// 5
+// умножаем до тех пор, пока i не станет 1000. + подсчет кол ва проходов
+// let i = 10;
+// let cout = 0
+// while (i <= 1000) {
+//      i = i * 3
+//      cout += 1
+// }
+// console.log(i);
+// console.log(cout);
+
+// 6
+//от 1 до 100
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// 7
+// от 11 до 33
+// for (let i = 11; i <= 33; i++) {
+//     console.log(i);
+// }
+
+// 8
+// четные от 0 до 100
+// for (let i = 0; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// 9
+// нечетные от 1 до 99
+// for (let i = 1; i <= 99; i += 2) {
+//     console.log(i);
+// }
+
+// 10
+// от 100 до 0
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// 11
+// вывести все элементы массива
+// function printArr(arr) {
+//     for (let mass = 0; mass <= arr.length; mass++) {
+//         if (arr[mass] != undefined) { // если не равно undefined
+//             console.log(arr[mass]);
+//         }
+//     }
+// }
+
+// let arr = ['a', 4, 'c', 'd', 'e'];
+// printArr(arr)
+
+// 12
