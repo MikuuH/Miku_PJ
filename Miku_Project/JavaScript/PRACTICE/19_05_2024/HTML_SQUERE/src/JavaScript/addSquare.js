82defafe757455e3cb41ef38e7242c0c1ee9222b
Miku <39mkui@gmail.com>
2024-05-19 16:39:09 +0300

    add squere

--- Miku_Project/JavaScript/PRACTICE/19_05_2024/HTML_SQUERE/src/JavaScript/addSquare.js
+++ Miku_Project/JavaScript/PRACTICE/19_05_2024/HTML_SQUERE/src/JavaScript/addSquare.js
@@ -1,11 +1,24 @@
 document.getElementById('createButton').addEventListener('click', function() {
+    let randomRGB = [
+        '#66cdaa',
+        '#667b68',
+        '#ccff00'
+    ]
 
     var square = document.createElement('div');
+
     square.style.width = '40px';
     square.style.height = '40px';
     square.style.marginTop = '10px'
-    square.style.background = '#ff0000';
+    square.style.background = randomRGB[Math.floor(Math.random() * randomRGB.length)];
 
     // Добавление нового абзаца в контейнер
     document.getElementById('container').appendChild(square);
+
+    var paragraph = document.createElement('p');
+    paragraph.textContent = square.id = "Id: " + Date.now() + String(Math.floor(Math.random() * 100));
+
+    container.appendChild(square);
+    container.appendChild(paragraph);
+
 });
