1fe3955a69390b28f283773d2b96d25c2e51a9b2
miku <39mkui@gmail.com>
2024-05-31 16:50:51 +0300

    modified:   .vscode/settings.json modified:   Miku_Project/CPP/ACADEMY/HOMEWORK/13_04_2024/educational.cpp new file:   Miku_Project/CPP/ACADEMY/HOMEWORK/13_04_2024/output/educational

--- Miku_Project/CPP/ACADEMY/HOMEWORK/13_04_2024/educational.cpp
+++ Miku_Project/CPP/ACADEMY/HOMEWORK/13_04_2024/educational.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-
-
-
 class educational {
     public:
         int audience;
@@ -14,33 +11,48 @@ class educational {
 
         educational(
             int audienceEducational,
-            string capacityEducational
+            string capacityEducational,
             string statusEducational
-        ) {
-            audience = audienceEducational;
-            capacity = capacityEducational;
-            status = statusEducational;
-        };
+    
+        ) : audience(audienceEducational),
+            capacity(capacityEducational),
+            status(statusEducational) {}
 
-        
+        void info() {
+            cout << audience << endl;
+            cout << capacity << endl;
+            cout << status << endl;
+        };
 };
 
 
 class lecture : public educational {
+    public:
+        using educational::educational;
 
 };
 
 class computer : public educational {
+    public:
+        using educational::educational;
 
 };
 
 class labarotoriy : public educational {
+    public:
+        using educational::educational;
 
 };
 
-
-
 int main() {
+
+    cout << "Hi! My name is Aimee! Translated from Japanese, it means 'beautiful love'.I got a wonderful homework assignment from @MikuSv0! Enjoy watching!";
+
+    lecture lect(12, "yes", "yea");
+    lect.info();
+
+    computer lab(44, "ff", "ff");
+    lab.info();
 
     return 0;
 }
