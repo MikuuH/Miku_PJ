3315699f35c799b2be173dc43e1d2127f5263e67
miku <39mkui@gmail.com>
2024-06-23 17:17:03 +0300

    -

--- Miku_Project/JavaScript/ACADEMY/23_06_2024/html/src/JavaScript/main.js
+++ Miku_Project/JavaScript/ACADEMY/23_06_2024/html/src/JavaScript/main.js
@@ -1,6 +1,6 @@
 // Fetch API
 document.getElementById('fetch').addEventListener('click', function() {
-    fetch("https://reqres.in/api/unknown/2")
+    fetch("https://reqres.in/api/{resource}/10")
         .then(response => response.json())
         .then(data => console.log("Fetch API Response:", data))
         .catch(error => console.error('Fetch Error:', error));
@@ -10,7 +10,7 @@ document.getElementById('fetch').addEventListener('click', function() {
 document.getElementById('xhr').addEventListener('click', function() {
     var xhr = new XMLHttpRequest();
     // xhr.open("GET", "https://reqres.in/api/users/2");
-    xhr.open('GET', 'https://reqres.in/api/users', false); // Третий параметр false указывает на синхронность запроса
+    xhr.open('GET', 'https://reqres.in/api/users?page=12&per_page=33', false); // Третий параметр false указывает на синхронность запроса
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4) {
